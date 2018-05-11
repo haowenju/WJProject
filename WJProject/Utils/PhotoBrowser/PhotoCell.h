0da61b9f2bbb70dda3bfbe0c61c6c4fd4cc831bb
haowenju <1069239913@qq.com>
2018-05-11 11:02:07 +0800

    增加图片浏览器

---
+++ WJProject/Utils/PhotoBrowser/PhotoCell.h
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.h
+//  WJProject
+//
+//  Created by 郝文举 on 2018/5/10.
+//  Copyright © 2018年 wenju hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCell : UICollectionViewCell
+
+@property (strong, nonatomic) NSString *url;
+
+@end
